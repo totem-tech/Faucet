5c7909ecfad74317ab1a0c7d3b03cfa31bd4497d
Chris D'Costa <chris.dcosta@meek.io>
2019-08-31 12:28:44 +0200

    Fix create from bytes problem in naclHelper.js and refactor #85 #82

--- handleFaucetTransfer.js
+++ handleFaucetTransfer.js
@@ -1,9 +1,9 @@
-import { decrypt, encryptionKeypair, signingKeyPair, verifySignature, getKeyPair } from './src/utils/naclHelper'
+import { decrypt, encryptionKeypair, signingKeyPair, verifySignature, keyInfoFromKeyData } from './src/utils/naclHelper'
 import { txHandler } from './txHandler'
 
 // Queue transactions
 let api;
-let amount, uri, keyData, walletAddress, publicKey, secretKey, serverName, external_publicKey, external_signPublicKey, external_serverName
+let amount, keyData, walletAddress, serverName, external_publicKey, external_signPublicKey, external_serverName, encryption_keypair, signature_keypair, printSensitiveData
 // Reads environment variables and generate keys if needed
 const setVariables = () => {
     amount = eval(process.env.amount) || 100000
@@ -18,30 +18,37 @@ const setVariables = () => {
         return 'Missing environment variable(s): "external_publicKey", "external_signPublicKey" or "external_serverName"'
     }
 
+    // Key data must be 96 bytes hex
     if (!process.env.keyData) return 'Missing environment variable: "keyData"'
-
-    uri = process.env.uri
-    if (!uri) return 'Missing environment variable: "uri"'
 
     // Prevent generating keys when not needed
     if (keyData === process.env.keyData) return
+
     // Key pairs of this server
     keyData = process.env.keyData
-    const keyPair = encryptionKeypair(keyData)
-    walletAddress = keyPair.walletAddress
-    publicKey = keyPair.publicKey
-    secretKey = keyPair.secretKey
+    const keyDataBytes = keyInfoFromKeyData(keyData)
+    const encryptionKeyPair = encryptionKeypair(keyData)
+    const signatureKeyPair = signingKeyPair(keyData)
+    walletAddress = keyDataBytes.walletAddress
+    encryption_keypair = encryptionKeyPair
+    signature_keypair = signatureKeyPair
 }
 // Set variables on start
 const err = setVariables()
 if (err) throw new Error(err)
-console.log('keyData: ', keyData)
-console.log('walletAddress: ', walletAddress)
-// TODO The following should not display the secret key.
-console.log('Encryption KeyPair base64 encoded: \n' + JSON.stringify({ publicKey, secretKey }, null, 4))
-console.log('serverName: ', serverName)
-console.log('external_publicKey base64 encoded: ', external_publicKey)
-console.log('external_serverName: ', external_serverName, '\n')
+
+printSensitiveData = process.env.printSensitiveData === "YES"
+
+if (printSensitiveData) {
+    console.log('serverName: ', serverName, '\n')
+    console.log('keyData: ', keyData, '\n')
+    console.log('walletAddress: ', walletAddress, '\n')
+    console.log('Encryption KeyPair base64 encoded: \n' + JSON.stringify(encryption_keypair, null, 4), '\n')
+    console.log('Signature KeyPair base64 encoded: \n' + JSON.stringify(signature_keypair, null, 4), '\n')
+    console.log('external_serverName: ', external_serverName,)
+    console.log('external_publicKey base64 encoded: ', external_publicKey, '\n')
+}
+
 export const setApi = polkadotApi => api = polkadotApi
 
 export const handleFaucetTransfer = (encryptedMsg, nonce, callback) => {
@@ -55,7 +62,7 @@ export const handleFaucetTransfer = (encryptedMsg, nonce, callback) => {
         external_publicKey,
         secretKey
     )
-    console.log('\ndecrypted', decrypted)
+    printSensitiveData && console.log('\ndecrypted', decrypted)
     if (!decrypted) return callback('Decryption failed')
 
     const minLength = 9
@@ -65,17 +72,18 @@ export const handleFaucetTransfer = (encryptedMsg, nonce, callback) => {
     const msgServerName = decryptedArr.slice(minLength, dataStart).join('')
     if (serverName !== msgServerName) return callback('Invalid data', msgServerName, serverName)
     const signature = decryptedArr.slice(sigStart).join('')
-    console.log('\nSignature:\n', signature)
-    console.log('\nexternal_signPublicKey:\n', external_signPublicKey)
+    printSensitiveData && console.log('\nSignature:\n', signature)
+    printSensitiveData && console.log('\nexternal_signPublicKey:\n', external_signPublicKey)
     const data = decryptedArr.slice(dataStart, sigStart).join('')
-    console.log('\nData:\n', data)
+    printSensitiveData && console.log('\nData:\n', data)
+    
     if (!verifySignature(data, signature, external_signPublicKey)) return callback('Signature verification failed')
 
     const faucetRequest = JSON.parse(data)
     if (faucetRequest.funded) return callback('Request already funded')
     if (!faucetRequest.address) return callback('Invalid address')
 
-    txHandler(api, process.env.uri, faucetRequest.address, amount, getKeyPair(keyData).secretKey32)
+    txHandler(api, faucetRequest.address, amount, keyInfoFromKeyData(keyData).first32Bytes, printSensitiveData)
         .catch(err => console.error('txHandler error: ', err) | callback(err))
     callback()
 }
