493f4001fae8de6e0eeee3938bc14f42b91da0c5
Toufiqur Chowdhury <htr.letun@gmail.com>
2023-06-27 02:19:37 +0300

    Updated babel & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,50 @@
 const webpack = require('webpack')
 const path = require('path');
 module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(md)$/,
-        loader: 'ignore-loader',
-      },
-    ]
-  },
-  // ignore nodejs modules that are not being used to avoid not found error
-  plugins: [
-    new webpack.IgnorePlugin(/abort-controller/),
-    new webpack.IgnorePlugin(/form-data/),
-    new webpack.IgnorePlugin(/nano/), // CouchDB Client
-    new webpack.IgnorePlugin(/node-localstorage/),
-    new webpack.IgnorePlugin(/node-fetch/),
-    new webpack.IgnorePlugin(/\@polkadot\/util-crypto/),
-    new webpack.IgnorePlugin(/\@polkadot\/util/),
-  ],
+	entry: {
+		main: ['babel-polyfill', './src/index.js'],
+	},
+	module: {
+		rules: [
+			// {
+			//   test: /\.(js)$/,
+			//   exclude: /node_modules/,
+			//   use: ['babel-loader']
+			// },
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							// Use the latest version of ECMAScript
+							[
+								'@babel/preset-env',
+								{ targets: 'last 2 versions' },
+							],
+							// Use the React preset for JSX support
+							'@babel/preset-react',
+						],
+					},
+				},
+			},
+			{
+				test: /\.(md)$/,
+				loader: 'ignore-loader',
+			},
+		]
+	},
+	// ignore nodejs modules that are not being used to avoid not found error
+	plugins: [
+		new webpack.IgnorePlugin(/abort-controller/),
+		new webpack.IgnorePlugin(/form-data/),
+		new webpack.IgnorePlugin(/nano/), // CouchDB Client
+		new webpack.IgnorePlugin(/node-localstorage/),
+		new webpack.IgnorePlugin(/node-fetch/),
+		new webpack.IgnorePlugin(/\@polkadot\/util-crypto/),
+		new webpack.IgnorePlugin(/\@polkadot\/util/),
+	],
 }
 
 
